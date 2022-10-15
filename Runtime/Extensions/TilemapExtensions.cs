b9ba28c2080dbc586aafecc3c526d8eea3a08b86
Nuno "Omega Leo" Diogo <nunodiogo@omegaleo.pt>
2022-10-15 17:19:27 +0100

    new extensions

---
+++ Runtime/Extensions/TilemapExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace OmegaLeo.Toolbox.Runtime.Extensions
+{
+    public static class TilemapExtensions
+    {
+        /// <summary>
+        /// Get all positions of filled tiles in the <paramref name="tilemap"/>
+        /// </summary>
+        /// <param name="tilemap"></param>
+        /// <returns></returns>
+        public static IEnumerable<Vector3Int> GetTilePositions(this Tilemap tilemap)
+        {
+            foreach (var pos in tilemap.cellBounds.allPositionsWithin)
+            {
+                if (tilemap.GetTile(pos) != null)
+                {
+                    yield return pos;
+                }
+            }
+        }
+        
+        /// <summary>
+        /// Get tilemap related position of an object
+        /// </summary>
+        /// <param name="tilemap"></param>
+        /// <returns></returns>
+        public static Vector3Int GetTilePosition(this Tilemap tilemap, Vector3 position)
+        {
+            return tilemap.WorldToCell(position);
+        }
+    }
+}
