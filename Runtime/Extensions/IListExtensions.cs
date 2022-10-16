2987c17c3e4cb9565669bf8670e2c590b3202bb7
Nuno "Omega Leo" Diogo <nunodiogo@omegaleo.pt>
2022-10-16 22:30:14 +0100

    More list extensions

--- Runtime/Extensions/IListExtensions.cs
+++ Runtime/Extensions/IListExtensions.cs
@@ -94,5 +94,19 @@ namespace OmegaLeo.Toolbox.Runtime.Extensions
         
             return values;
         }
+        
+        public static IEnumerable<TSource> DistinctBy<TSource, TKey>
+            (this IEnumerable<TSource> source, Func<TSource, TKey> keySelector)
+        {
+            HashSet<TKey> seenKeys = new HashSet<TKey>();
+            foreach (TSource element in source)
+            {
+                if (seenKeys.Add(keySelector(element)))
+                {
+                    yield return element;
+                }
+            }
+        }
+
     }
 }
