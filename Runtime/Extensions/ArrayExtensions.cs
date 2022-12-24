10bd34dd2ff47a3b1aafb84e7688818d5e80fe8d
OmegaLeo <nunodiogo@omegaleo.pt>
2022-12-24 16:37:42 +0000

    Added new Array Extensions

---
+++ Runtime/Extensions/ArrayExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace OmegaLeo.Toolbox.Runtime.Extensions
+{
+    public static class ArrayExtensions
+    {
+        // Idea obtained from TaroDev's video on things to do in Unity - https://youtu.be/Ic5ux-tpkCE?t=302
+        public static T Next<T>(this T[] array, int currentIndex)
+        {
+            return array[currentIndex++ % array.Length];
+        }
+    }
+}
