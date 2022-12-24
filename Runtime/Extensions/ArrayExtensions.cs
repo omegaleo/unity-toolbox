f7a76f46c109109294cd9c798c9b4d4a2fc35bbb
OmegaLeo <nunodiogo@omegaleo.pt>
2022-12-24 16:43:42 +0000

    update to Next()

--- Runtime/Extensions/ArrayExtensions.cs
+++ Runtime/Extensions/ArrayExtensions.cs
@@ -3,7 +3,7 @@
     public static class ArrayExtensions
     {
         // Idea obtained from TaroDev's video on things to do in Unity - https://youtu.be/Ic5ux-tpkCE?t=302
-        public static T Next<T>(this T[] array, int currentIndex)
+        public static T Next<T>(this T[] array, ref int currentIndex)
         {
             return array[currentIndex++ % array.Length];
         }
