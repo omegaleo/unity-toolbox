8e5a4d4091aadb5866da6b7611331e6f476388ea
OmegaLeo <nunodiogo@omegaleo.pt>
2023-02-17 16:54:44 +0000

    changes to SceneSelectionOverlay.cs

--- Editor/Overlays/SceneSelectionOverlay.cs
+++ Editor/Overlays/SceneSelectionOverlay.cs
@@ -1,11 +1,14 @@
 /*
     Disclaimer: I do not own this code, only did some changes here and there, original code comes from 
     Warped Imagination's video on Scene Selection Overlay - https://www.youtube.com/watch?v=yqneLnM8syk
+    and also Organize Generic Unity Menus with Submenus - https://www.youtube.com/watch?v=zeF3YtHtvf4
     Support them by going to their channel and giving them some love for their awesome tutorials.
 */
 
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using OmegaLeo.Toolbox.Editor.Helpers;
 using OmegaLeo.Toolbox.Runtime.Extensions;
 using UnityEditor;
@@ -59,24 +62,32 @@ namespace OmegaLeo.Toolbox.Editor.Overlays
 
                     var scene = AssetDatabase.LoadAssetAtPath<SceneAsset>(path);
 
-                    menu.AddItem(new GUIContent(scene.name), string.Equals(currentScene.name, scene.name), () => OpenScene(currentScene, path));
+                    if (String.CompareOrdinal(currentScene.name, scene.name) == 0)
+                    {
+                        menu.AddDisabledItem(new GUIContent(scene.name));
+                    }
+                    else
+                    {
+                        menu.AddItem(new GUIContent(scene.name + "/Single"), false, () => OpenScene(currentScene, path, OpenSceneMode.Single));
+                        menu.AddItem(new GUIContent(scene.name + "/Additive"), false, () => OpenScene(currentScene, path, OpenSceneMode.Additive));
+                    }
                 }
                 
                 menu.ShowAsContext();
             }
 
-            private void OpenScene(Scene currentScene, string path)
+            private void OpenScene(Scene currentScene, string path, OpenSceneMode mode)
             {
                 if (currentScene.isDirty)
                 {
                     if (EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
                     {
-                        EditorSceneManager.OpenScene(path);
+                        EditorSceneManager.OpenScene(path, mode);
                     }
                 }
                 else
                 {
-                    EditorSceneManager.OpenScene(path);
+                    EditorSceneManager.OpenScene(path, mode);
                 }
             }
         }
