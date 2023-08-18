134c328d78de4ce3683e4adb4b31b3c9b8001048
OmegaLeo <nunodiogo@omegaleo.pt>
2023-08-18 22:51:40 +0100

    Added Method Button Attribute

---
+++ Editor/Editors/MonoBehaviourCustomEditor.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using OmegaLeo.Toolbox.Attributes;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using Object = UnityEngine.Object;
+
+namespace OmegaLeo.Toolbox.Editor.Omega_Leo_Toolbox.Editor.Editors
+{
+    [CustomEditor(typeof(MonoBehaviour), true), CanEditMultipleObjects] // Target all MonoBehaviours and descendants
+public class MonoBehaviourCustomEditor : UnityEditor.Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector(); // Draw the normal inspector
+
+        // Get the type descriptor for the MonoBehaviour we are drawing
+        var type = target.GetType();
+
+        HandleMethods(type);
+    }
+
+    private void HandleMethods(Type type)
+    {
+        // Iterate over each private or public instance method (no static methods atm)
+        foreach (var method in type.GetMethods(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance |
+                                               BindingFlags.FlattenHierarchy))
+        {
+            // make sure it is decorated by our custom attribute
+            var attributes = method.GetCustomAttributes(typeof(MethodButtonAttribute), true);
+            if (attributes.Length > 0)
+            {
+                var attribute = attributes.Select(x => x as MethodButtonAttribute)
+                    .FirstOrDefault(x => x.methodName == method.Name);
+                if (GUILayout.Button(attribute.buttonName))
+                {
+                    // If the user clicks the button, invoke the method immediately.
+                    // There are many ways to do this but I chose to use Invoke which only works in Play Mode.
+                    method.Invoke(target, null);
+                }
+            }
+        }
+    }
+}
+}
---
+++ Runtime/Attributes/MethodButtonAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Reflection;
+
+namespace OmegaLeo.Toolbox.Attributes
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class MethodButtonAttribute : Attribute {
+        public string methodName;
+        public string buttonName;
+        public bool useValue;
+        public BindingFlags flags;
+
+        public MethodButtonAttribute(string methodName, string buttonName, bool useValue, BindingFlags flags = BindingFlags.Public | BindingFlags.Instance) {
+            this.methodName = methodName;
+            this.buttonName = buttonName;
+            this.useValue = useValue;
+            this.flags = flags;
+        }
+        public MethodButtonAttribute(string methodName, bool useValue, BindingFlags flags) : this (methodName, methodName, useValue, flags) {}
+        public MethodButtonAttribute(string methodName, bool useValue) : this (methodName, methodName, useValue) {}
+        public MethodButtonAttribute(string methodName, string buttonName, BindingFlags flags) : this (methodName, buttonName, false, flags) {}
+        public MethodButtonAttribute(string methodName, string buttonName) : this (methodName, buttonName, false) {}
+        public MethodButtonAttribute(string methodName, BindingFlags flags) : this (methodName, methodName, false, flags) {}
+        public MethodButtonAttribute(string methodName) : this (methodName, methodName, false) {}
+    }
+}
