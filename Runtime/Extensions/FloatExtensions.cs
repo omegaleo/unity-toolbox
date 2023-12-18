abc88a686294be27683111b0881028f1d1327ba4
Nuno 'Omega Leo' Diogo <nunodiogo@omegaleo.pt>
2023-12-18 15:12:36 +0000

    Added some float extensions

---
+++ Runtime/Extensions/FloatExtensions.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace OmegaLeo.Toolbox.Runtime.Extensions
+{
+    public static class FloatExtensions
+    {
+        public static float Round(this float value)
+        {
+            return Mathf.Round(value);
+        }
+
+        public static float RoundToInt(this float value)
+        {
+            return Mathf.RoundToInt(value);
+        }
+    }
+}
