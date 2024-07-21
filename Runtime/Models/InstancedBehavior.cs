dfba7f73142e27fdb25b48f5ec7e615325eedc8c
Nuno "Omega Leo" Diogo <nunodiogo@omegaleo.pt>
2024-07-21 13:08:30 +0100

    undid mistake in InstancedBehavior.cs

--- Runtime/Models/InstancedBehavior.cs
+++ Runtime/Models/InstancedBehavior.cs
@@ -10,7 +10,7 @@ namespace OmegaLeo.Toolbox.Runtime.Models
     [Documentation("InstancedBehavior<T>", "Create an instanced MonoBehaviour of type T which can than be called by using <ClassName>.instance")]
     public class InstancedBehavior<T> : MonoBehaviour where T: Component
     {
-        private static T instance;
+        public static T instance;
 
         protected virtual void Awake()
         {
